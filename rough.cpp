e9d8682de66775a3491051bf14e90b0a42de506f
yuvaraaj <2003yuvaraaj@gmail.com>
2024-09-23 18:00:39 +0530

    2nd try

--- rough.cpp
+++ rough.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<cstdio>
+#include<iostream>
 int main()
 {
-    int x;
-    cin>>x;
-    cout<<"the value of x is "<<"\n"<<x;
+    int a;
+    scanf("%d",&a);
+    std::cout<<"the value of a is "<<a;
     return 0;
 }
